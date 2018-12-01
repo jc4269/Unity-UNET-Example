eed773d51341a8c7188c438d67a368bff22d099d
Jai Carlton <jai.carlton@gmail.com>
2018-12-01 22:43:02 +1000

    Added health bar and damage function but isn't networked.

--- Unity-UNET-Example/Assets/Scripts/Bullet.cs
+++ Unity-UNET-Example/Assets/Scripts/Bullet.cs
@@ -6,6 +6,15 @@ public class Bullet : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        //Debug.Log("Hit");
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
         Destroy(gameObject);
     }
+
+
 }
---
+++ Unity-UNET-Example/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    public RectTransform HealthBar;
+
+
+    public void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        HealthBar.sizeDelta = new Vector2(CurrentHealth, HealthBar.sizeDelta.y);
+    }
+}
