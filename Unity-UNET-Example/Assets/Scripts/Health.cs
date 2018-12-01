4a621089b41b134f5a0cdf46c8a2e2e42bef7126
Jai Carlton <jai.carlton@gmail.com>
2018-12-01 23:23:36 +1000

    Added spawn points and make players choose a spawn point at the start and then randomly choose one to respawn to on death.

--- Unity-UNET-Example/Assets/Scripts/Health.cs
+++ Unity-UNET-Example/Assets/Scripts/Health.cs
@@ -12,9 +12,17 @@ public class Health : NetworkBehaviour {
     public RectTransform HealthBar;
     public bool DestroyOnDeath;
 
+    private NetworkStartPosition[] spawnPoints;
+
     public void Start()
     {
         CurrentHealth = MaxHealth;
+
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+
     }
 
     public void TakeDamage(int amount)
@@ -54,7 +62,19 @@ public class Health : NetworkBehaviour {
     {
         if (isLocalPlayer)
         {
-            transform.position = Vector3.zero;
+            //transform.position = Vector3.zero; //center of the map spawn.
+
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick a spawn point at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
         }
     }
 }
