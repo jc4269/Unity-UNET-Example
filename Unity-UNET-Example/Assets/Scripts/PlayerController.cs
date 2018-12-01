d6bad348903abffb088b4556ccbb806a113688e8
Jai Carlton <jai.carlton@gmail.com>
2018-12-01 22:04:38 +1000

    Added multiplayer shooting.

--- Unity-UNET-Example/Assets/Scripts/PlayerController.cs
+++ Unity-UNET-Example/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     void Update()
     {
         if (!isLocalPlayer)
@@ -17,10 +20,35 @@ public class PlayerController : NetworkBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
     }
 
     public override void OnStartLocalPlayer()
     {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
+        GetComponent<MeshRenderer>().material.color = Color.magenta;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        //NOTE: remember to initiate everything before spawning as it sends variables to the clients like velocity.
+        //Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
     }
 }
